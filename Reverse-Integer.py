1e63dce10a11c8633e8451efe0b0702ffbe832da
phkeerthana5 <phkeerthana5@gmail.com>
2026-09-10 11:49:37 +0530

    Implement reverse integer function in Python

---
+++ Reverse-Integer.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverse(self, x):
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        result = 0
+
+        while x:
+            digit = x % 10
+            result = result * 10 + digit
+            x //= 10
+
+        result *= sign
+
+        if result < -2**31 or result > 2**31 - 1:
+            return 0
+
+        return result
